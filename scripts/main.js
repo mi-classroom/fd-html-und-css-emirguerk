93fb7efd14408b6c8590dd95bdaf54d6d23b9b11
emirguerk <emirhan.guerkan@smail.th-koeln.de>
2025-06-21 19:10:59 +0200

    search events

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,17 @@ document.querySelector('[data-js-menu-button]').addEventListener('click', () => 
 document.querySelector('nav [data-js-close-button]').addEventListener('click', () => {
     document.querySelector('[data-js-header-nav]').classList.remove('is-active')
 })
+
+document.querySelector('[data-js-searach-button]').addEventListener('click', () => {
+    document.querySelector('[data-js-header-search]').classList.add('is-active')
+})
+
+document.querySelector('form [data-js-close-button]').addEventListener('click', () => {
+    document.querySelector('[data-js-header-search]').classList.remove('is-active')
+})
+
+document.querySelector('[data-js-header-search]').addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(`Search: ${event.target[1].value}`)
+    document.querySelector('[data-js-header-search]').classList.remove('is-active')
+})
